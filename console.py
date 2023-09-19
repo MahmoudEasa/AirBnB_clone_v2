6b34c3614a21f593f28801662c4b2fe3e8d84e6b
MahmoudEasa <mu01011422865@gmail.com>
2023-09-19 16:37:48 +0000

    Fix console.py 1

--- console.py
+++ console.py
@@ -127,6 +127,7 @@ class HBNBCommand(cmd.Cmd):
                 try:
                     if spl[1][0] == '"' and spl[1][-1] == '"':
                         val = spl[1][1:-1].replace('_', ' ')
+                        val = val.replace('\\"', '"')
                     elif spl[1].isdigit():
                         val = int(spl[1])
                     elif spl[1].find('.') != -1:
