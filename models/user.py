ca3fa4be01272d9f8f2f93eb313a3101dcc04d6f
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-19 23:11:33 -0700

    solve task no 7

--- models/user.py
+++ models/user.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String
 
 
-class User(BaseModel):
-    """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+class User(BaseModel, Base):
+
+    """This class defines a user by various attributes
+    Attributes:
+        __tablename__: Table name
+        email: email address
+        first_name: first name of user
+        last_name: last name of user
+    """
+
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
