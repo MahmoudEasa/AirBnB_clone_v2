c4be20e7816313e4be9b6d8bcad656e0b2f5922b
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-20 18:25:12 -0700

    modify State

--- models/state.py
+++ models/state.py
@@ -11,15 +11,14 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        cities = relationship("City", cascade="all, delete", backref="state")
-    else:
+    cities = relationship("City", cascade="delete", backref="state")
+    
+    if getenv("HBNB_TYPE_STORAGE") != 'db':
         @property
         def cities(self):
             """ getter attribute cities that returns the list of City """
             city_list = []
-            for city in (models.storage.all(City).values()):
+            for city in list(models.storage.all(City).values()):
                 if city.state.id == self.id:
                     city_list.append(city)
             return city_list
