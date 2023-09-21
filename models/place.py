c65056bace77fb5be8b668a03f77c3d1489c45e5
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-09-20 17:11:57 -0700

    solve task

--- models/place.py
+++ models/place.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
-from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from models.base_model import BaseModel, Base
+from models.amenity import Amenity
+from models.review import Review
+from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+import os
+import models
+
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id'),
+                             primary_key=True, nullable=False),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id'),
+                             primary_key=True, nullable=False))
 
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = "places"
 
@@ -19,3 +32,35 @@ class Place(BaseModel):
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
     amenity_ids = []
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False)
+        reviews = relationship("Review", cascade="all, delete",
+                               backref="place")
+
+    else:
+        @property
+        def amenities(self):
+            """Getter attribute amenities"""
+            amenity_objs = []
+            for amenity_id in self.amenity_ids:
+                amenity_obj = models.storage.get("Amenity", amenity_id)
+                if amenity_obj:
+                    amenity_objs.append(amenity_obj)
+            return amenity_objs
+
+        @amenities.setter
+        def amenities(self, obj):
+            """Setter attribute amenities"""
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)
+
+        @property
+        def reviews(self):
+            """Getter attribute reviews"""
+            review_objs = []
+            for review in models.storage.all("Review").values():
+                if review.place_id == self.id:
+                    review_objs.append(review)
+            return review_objs
