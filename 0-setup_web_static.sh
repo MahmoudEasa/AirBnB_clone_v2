5e3263797d0a73110d0e8efc55c7539ac00bce6a
LobnaMuhamed <mohamedlobna306@gmail.com>
2023-10-05 17:01:19 -0700

    solve task no 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-<<<<<<< HEAD
 # Sets up a web server for deployment of web_static.
 
 apt-get update
@@ -14,20 +13,11 @@ mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
 
 echo "<html>
-=======
-# Script that sets up your web servers for the deployment of web_static.
-
-SITE_FILE=/etc/nginx/sites-available/default
-END_POINT='\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n'
-HTML_CONTENT=\
-"<html>
->>>>>>> 1a1e941993bac195515a6eb20020724a6656fd93
   <head>
   </head>
   <body>
     Holberton School
   </body>
-<<<<<<< HEAD
 </html>" > /data/web_static/releases/test/index.html
 
 ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -38,24 +28,3 @@ nginx_config="/etc/nginx/sites-available/default"
 sed -i '/^server {/a \\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' "$nginx_config"
 
 service nginx restart
-=======
-</html>"
-
-CHECK_HBNB=$(grep 'location /hbnb_static' < "$SITE_FILE")
-
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p '/data/web_static/releases/test/'
-sudo mkdir -p '/data/web_static/shared/'
-sudo touch '/data/web_static/releases/test/index.html'
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-echo "$HTML_CONTENT" > '/data/web_static/releases/test/index.html'
-
-if [ -z "$CHECK_HBNB" ]; then
-	sudo sed -i "s@^\tserver_name _;@&$END_POINT@" $SITE_FILE
-fi
-
-sudo service nginx restart
->>>>>>> 1a1e941993bac195515a6eb20020724a6656fd93
