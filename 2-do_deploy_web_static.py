5d63d8a9bfc1ae6893eb950f27e8c291f8c989df
MahmoudEasa <mu01011422865@gmail.com>
2023-10-07 07:54:19 +0000

    Fix 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -24,13 +24,16 @@ def do_deploy(archive_path):
 
     archive_name = archive_path.split('/')[-1]
     name = archive_name.split('.')[0]
-    uncompress_path = f"/data/web_static/releases/{name}"
-    uncompress_cmd = f"tar -xzf /tmp/{archive_name} -C {uncompress_path}"
-    create_path = f"mkdir -p {uncompress_path}"
-    remove_archive = f"rm -rf /tmp/{archive_name}"
-    move = f"mv {uncompress_path}/web_static/* {uncompress_path}"
-    remove_web_static = f"rm -rf {uncompress_path}/web_static"
-    link_archive = f"ln -s {uncompress_path} /data/web_static/current"
+    uncompress_path = "/data/web_static/releases/{}".format(name)
+    uncompress_cmd = "sudo tar -xzf /tmp/{} -C {}"\
+            .format(archive_name, uncompress_path)
+    create_path = "sudo mkdir -p {}".format(uncompress_path)
+    remove_archive = "sudo rm -rf /tmp/{}".format(archive_name)
+    move = "sudo mv {}/web_static/* {}"\
+            .format(uncompress_path, uncompress_path)
+    remove_web_static = "sudo rm -rf {}/web_static".format(uncompress_path)
+    link_archive = "sudo ln -s {} /data/web_static/current"\
+            .format(uncompress_path)
 
     if put(archive_path, '/tmp/').failed is True:
         return (False)
@@ -44,8 +47,9 @@ def do_deploy(archive_path):
         return (False)
     if run(remove_web_static).failed is True:
         return (False)
-    if run("rm -rf /data/web_static/current").failed is True:
+    if run("sudo rm -rf /data/web_static/current").failed is True:
         return (False)
     if run(link_archive).failed is True:
         return (False)
+    print("New version deployed!")
     return (True)
