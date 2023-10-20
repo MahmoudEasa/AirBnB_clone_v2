a73c88e8b7f3fcd985fd036b74a3003771ae6594
MahmoudEasa <mu01011422865@gmail.com>
2023-10-20 21:45:24 +0000

    10-hbnb_filters.py

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python3
+""" Start a Flask web application
 """
-"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.amenity import Amenity
 
 
+app = Flask(__name__)
 
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    """ Display a HTML page for hbnb_filters """
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template('10-hbnb_filters.html',
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """ Remove the current SQLAlchemy Session """
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
